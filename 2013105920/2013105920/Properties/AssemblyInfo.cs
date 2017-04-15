cad639df96a05feba916b687c5ef318f1819c0f8
Marco Antonio Castillo Gomez <Marco Antonio Castillo Gomez>
2017-04-15 13:06:35 -0500

    primer commit

--- 2013105920/2013105920/Properties/AssemblyInfo.cs
+++ 2013105920/2013105920/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
-[assembly: Guid("9374d2cf-c2a8-4020-be8d-416c721b186f")]
+[assembly: Guid("6018ff6d-a332-4202-a540-d11c21f67716")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
